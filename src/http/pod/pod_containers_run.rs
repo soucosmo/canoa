ad2dd34a1ac63fe7bb977dde7159d8d470674670
Cosmo André <soucosmomoraes@gmail.com>
2024-08-26 00:41:31 -0300

    has been added the image pull and container start

--- src/http/pod/pod_containers_run.rs
+++ src/http/pod/pod_containers_run.rs
@@ -1,11 +1,14 @@
-use bollard::container::{CreateContainerOptions, Config};
+use bollard::container::{CreateContainerOptions, Config, StartContainerOptions};
 use bollard::image;
+
+//use futures_util::StreamExt;
 use std::collections::HashMap;
 use crate::types::AppState;
 use actix_web::{web::Data, HttpResponse, Responder};
 use super::HttpServer;
 use std::sync::Mutex;
 use bollard::Docker;
+use futures_util::stream::StreamExt;
 
 impl HttpServer {
     pub async fn pod_containers_run(app_state: Data<Mutex<AppState>>, docker: Data<Docker>) -> impl Responder {
@@ -23,12 +26,34 @@ impl HttpServer {
             let image = pod.image.clone();
     
             let image = image.as_str();
+
+            //let mut buf = bytes::Bytes::new();
+
+            let image_options = image::CreateImageOptions {
+                from_image: image,
+                ..Default::default()
+            };
+
+            let mut imagepull = docker.create_image(Some(image_options), None, None);
+            
+
+            while let Some(response) = imagepull.next().await {
+                match response {
+                    Ok(res) => {
+                        println!("{:?}", res.progress_detail);
+                    }
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+                }
+            }
+        
     
             for i in 0..pod.replicas {
                 let name = format!("{}-{}", pod.name, i);
     
                 let options = CreateContainerOptions {
-                    name,
+                    name: name.clone(),
                     //platform: Some("linux/amd64"),
                     platform: None,
                 };
@@ -47,20 +72,22 @@ impl HttpServer {
                     attach_stderr: Some(true),
                     open_stdin: Some(true),
                     tty: Some(true),
+                    
                     ..Config::default()
                 };
 
-                let image_options = image::CreateImageOptions {
-                    from_image: image,
-                    ..Default::default()
-                };
-                
-                let mut buf = bytes::Bytes::new();
-
-                let _ = docker.create_image(Some(image_options), None, None);
-    
                 match docker.create_container(Some(options), config).await {
-                    Ok(_) => println!("Sucesss"),
+                    Ok(_) => {
+                        println!("Sucesss");
+                        let res = docker.start_container(name.as_str(), None::<StartContainerOptions<String>>).await;
+
+                        match res {
+                            Ok(_) => println!("Sucesso ao start"),
+                            Err(e) =>  println!("erro start {}", e)
+                        }
+
+
+                    },
                     Err(e) => println!("Error: {e}"),
                 }
             }
